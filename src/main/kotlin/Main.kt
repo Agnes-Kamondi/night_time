f9afcc2a92c97cfca7ba27cd1c51efcdc733c8bb
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-02-11 17:41:55 +0300

    added a kotlin file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+    var name : String = "Agnes Auma"
+    println(name)
+    var age : Int = 21
+    println(age)
+    var study = "Lovelace"
+    println(study)
+    var country = "Kenya"
+    println(country)
+    var weight = 61
+    println(weight)
+    var kenyan = true
+    println(kenyan)
+    val akirachix =arrayOf("Lovelace","Ada","Anita")
+    println(akirachix)
+    var wishes : String = "I want to be the best version of myself"
+    println(wishes)
+    var isKotlinFun : Boolean = true
+    println(isKotlinFun)
+    var isAkiraChixMyHome : Boolean = true
+    println(isAkiraChixMyHome)
+    var year: Short = 2024
+    println(year)
+    var phonNumber : String = "+254115052075"
+    println(phonNumber)
+    var letter :Char = 'A'
+    println(letter)
+    var daytime : Boolean = false
+    println(daytime)
+
+
+
+
+
+
+}
